37214cd3210ba3aeb1ab0083d5616fc466b2d9b4
ApinatSwu <apinat.ppx@g.swu.ac.th>
2026-02-03 16:42:20 +0700

    ..

--- Lab/app/src/main/java/com/example/a178lablearnandroid/ListMainActivity.kt
+++ Lab/app/src/main/java/com/example/a178lablearnandroid/ListMainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.a178lablearnandroid.ui.theme._178LabLearnAndroidTheme
 
 class ListMainActivity : ComponentActivity() {
@@ -18,30 +25,89 @@ class ListMainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _178LabLearnAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            ListScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ListScreen() {
+    Column(modifier = Modifier.background(Color.Red)
+        .padding(40.dp)
+        .padding(top=60.dp)
+        .fillMaxSize()
 
+    ) {
+        Column(modifier = Modifier.background(color=Color.Gray,shape = RoundedCornerShape(30.dp))
+            .padding(16.dp)
+
+            .fillMaxSize()
+
+        ) {
+            Column(modifier = Modifier.background(color=Color.White,shape =RoundedCornerShape(30.dp))
+                .padding(16.dp)
+                .fillMaxSize()
+
+            ) {
+                LazyColumn() {
+                    items(allKantoPokemon.size) {index ->
+                        val item = allKantoPokemon[index]
+                        Text(text = item.name, modifier = Modifier.padding(16.dp), fontSize = 36.sp)
+                    }
+                }
+            }
+
+        }
+
+    }
+
+}
+data class Pokemon(
+    val name: String,
+    val number: Int
+)
+
+val allKantoPokemon = listOf(
+    Pokemon("Bulbasaur", 1),
+    Pokemon("Ivysaur", 2),
+    Pokemon("Venusaur", 3),
+    Pokemon("Charmander", 4),
+    Pokemon("Charmeleon", 5),
+    Pokemon("Charizard", 6),
+    Pokemon("Squirtle", 7),
+    Pokemon("Wartortle", 8),
+    Pokemon("Blastoise", 9),
+    Pokemon("Caterpie", 10),
+    Pokemon("Metapod", 11),
+    Pokemon("Butterfree", 12),
+    Pokemon("Weedle", 13),
+    Pokemon("Kakuna", 14),
+    Pokemon("Beedrill", 15),
+    Pokemon("Pidgey", 16),
+    Pokemon("Pidgeotto", 17),
+    Pokemon("Pidgeot", 18),
+    Pokemon("Rattata", 19),
+    Pokemon("Raticate", 20),
+    Pokemon("Spearow", 21),
+    Pokemon("Fearow", 22),
+    Pokemon("Ekans", 23),
+    Pokemon("Arbok", 24),
+    Pokemon("Pikachu", 25),
+    Pokemon("Raichu", 26),
+    Pokemon("Sandshrew", 27),
+    Pokemon("Sandslash", 28),
+    Pokemon("Nidoran♀", 29),
+    Pokemon("Nidorina", 30),
+    Pokemon("Nidoqueen", 31),
+    Pokemon("Nidoran♂", 32),
+    Pokemon("Nidorino", 33),
+    Pokemon("Nidoking", 34),
+    Pokemon("Clefairy", 35),
+)
+
+// Tips: for image : https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/1.png
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    _178LabLearnAndroidTheme {
-        Greeting("Android")
-    }
+fun ListPreview() {
+    ListScreen()
 }
