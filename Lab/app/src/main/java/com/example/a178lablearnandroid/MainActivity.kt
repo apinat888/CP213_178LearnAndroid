0eaf274299dc3a0d889c319fbec919333f79467d
ApinatSwu <apinat.ppx@g.swu.ac.th>
2026-02-10 14:57:32 +0700

    10/2/69

--- Lab/app/src/main/java/com/example/a178lablearnandroid/MainActivity.kt
+++ Lab/app/src/main/java/com/example/a178lablearnandroid/MainActivity.kt
@@ -2,10 +2,9 @@ package com.example.a178lablearnandroid
 
 import android.content.Intent
 import android.os.Bundle
-import android.telephony.SignalStrength
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,9 +18,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -30,93 +27,149 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.a178lablearnandroid.ui.theme._178LabLearnAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+//        enableEdgeToEdge()
         setContent {
-
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.Gray)
-                .padding(32.dp)
-            ) {
+            Column(modifier = Modifier.fillMaxSize()
+                .background(color=Color.Cyan)
+                .padding(32.dp))  {
                 //hp
                 Box(modifier = Modifier
                     .fillMaxWidth()
                     .height(32.dp)
-                    .background(color = Color.White)
-
+                    .background(color=Color.White)
                 ) {
                     Text(
-                        text = "hp",
-                        modifier = Modifier
-                            .align(alignment = Alignment.CenterStart)
+                        text = "HP",
+                        modifier = Modifier.align(alignment= Alignment.CenterStart)
                             .fillMaxWidth(fraction = 0.78f)
-                            .background(color = Color.Magenta)
+                            .background(color=Color.LightGray)
                             .padding(8.dp)
+
+
                     )
+
+
                 }
+
                 //image
                 Image(
                     painter = painterResource(id = R.drawable.thomas),
-                    contentDescription = "profile",
+                    contentDescription = "Profile",
                     modifier = Modifier
-                        .size(size = 128.dp)
+                        .size(300.dp)
                         .align(Alignment.CenterHorizontally)
-                        .padding(top = 16.dp)
+                        .padding(top= 78.dp)
                         .clickable() {
-                            startActivity(Intent(this@MainActivity, ListMainActivity::class.java))
+                            startActivity(Intent(this@MainActivity, MainActivity2::class.java))
                         }
+
+
                 )
+
+
                 //status
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
                     horizontalArrangement = Arrangement.SpaceBetween
-                ) {
+
+                )
+                {
                     var str:Int by remember { mutableStateOf(10) }
                     var agi:Int by remember { mutableStateOf(10) }
                     var int:Int by remember { mutableStateOf(10) }
                     Column() {
-                        Button(onClick = {str = str+1}) {
+                        Button(onClick = {str = str+1 }) {
                             Text(text = "+", fontSize = 32.sp)
                         }
+
                         Text(text = "Str", fontSize = 32.sp)
                         Text(text = str.toString(), fontSize = 32.sp)
-                        Text(text = "-", fontSize = 32.sp,
-                            modifier = Modifier.clickable {
-                                str = str-1
-                            })
+//                        Text(text ="-",fontSize=32.sp,
+//                            modifier = Modifier.clickable {
+//                                str = str-1
+//                            })
+                        Button(onClick = {str = str-1 }) {
+                            Text(text = "-", fontSize = 32.sp)
+                        }
+
                     }
                     Column() {
-                        Button(onClick = {agi = agi+1}) {
+                        Button(onClick = {agi = agi+1 }) {
                             Text(text = "+", fontSize = 32.sp)
                         }
+
                         Text(text = "Agi", fontSize = 32.sp)
                         Text(text = agi.toString(), fontSize = 32.sp)
-                        Text(text = "-", fontSize = 32.sp,
-                            modifier = Modifier.clickable {
-                                agi = agi-1
-                            })
+
+                        Button(onClick = {agi = agi-1 }) {
+                            Text(text = "-", fontSize = 32.sp)
+                        }
                     }
                     Column() {
-                        Button(onClick = {int = int+1}) {
+                        Button(onClick = {int = int+10 }) {
                             Text(text = "+", fontSize = 32.sp)
                         }
+
                         Text(text = "Int", fontSize = 32.sp)
                         Text(text = int.toString(), fontSize = 32.sp)
-                        Text(text = "-", fontSize = 32.sp,
-                            modifier = Modifier.clickable {
-                                int = int-1
-                            })
+
+                        Button(onClick = {int = int-1 }) {
+                            Text(text = "-", fontSize = 32.sp)
+                        }
                     }
+
                 }
+
+
+
+
+
+
+
+
+
+
             }
         }
     }
+    override fun onStart() {
+        super.onStart()
+        Log.i("Lifecycle", "MainActivity : onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Lifecycle", "MainActivity : onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Lifecycle", "MainActivity : onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Lifecycle", "MainActivity : onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle", "MainActivity : onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Lifecycle", "MainActivity : onRestart")
+    }
+
+
 }
+
